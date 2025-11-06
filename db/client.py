873257b2ab60e8039dfa220402bf099d1a3c41e0
Lueyo <84575656+lueyo@users.noreply.github.com>
2025-11-06 13:26:41 +0100

    cambio de database

--- db/client.py
+++ db/client.py
@@ -1,4 +1,4 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from common.config import DATABASE_URL
 
-db_client = AsyncIOMotorClient(DATABASE_URL).get_database("videos_db")
+db_client = AsyncIOMotorClient(DATABASE_URL).get_database("randomyt_db")
