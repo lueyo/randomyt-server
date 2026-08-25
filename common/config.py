30f8bc237993503c8064e280bbaf168395eca2af
Lueyo <84575656+lueyo@users.noreply.github.com>
2026-08-25 18:35:16 +0200

    base de datos local

--- common/config.py
+++ common/config.py
@@ -4,7 +4,7 @@ import os
 load_dotenv()
 
 # Example configuration variables
-DATABASE_URL = os.getenv("DATABASE_URL", "mongodb://localhost:27017/")
+DATABASE_URL = os.getenv("DATABASE_URL_LOCAL", "mongodb://localhost:27017/")
 
 LIMIT_VIEWS = int(os.getenv("LIMIT_VIEWS", 2000))
 
